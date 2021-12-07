ed15d625fc1c23c57abd78ad12cdcf2198ddeb3c
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2021-12-07 23:09:53 +0300

    Adde dbasic functionality

--- html/scripts/knowledgeBase.js
+++ html/scripts/knowledgeBase.js
@@ -1,12 +1,36 @@
+const DEFAULT_ERROR = "Сформулируйте свой вопрос иначе."
 const TEMPLATES = [
 	[
 		[
-			"t1",
-			"t2",
-			"t3",
+			"как",
+			"кушать",
+			"пирок",
 		],
-		"test"
+		"Нужно кушать его ротиком"
 	],
 ]
 
-window.TEMPLATES = TEMPLATES;
+function getAnswer(question) {
+	if (question === undefined) return DEFAULT_ERROR;
+
+	let answer = DEFAULT_ERROR;
+	let letters = question
+		.toLowerCase()
+		.replace(/[^a-zA-Zа-яА-Я ]/g, "")
+		.split(' ');
+
+	TEMPLATES.forEach((data) => {
+		let template = data[0];
+		let currentAnswer = data[1];
+
+		letters.forEach((letter) => {
+			if (!template.includes(letter)) return DEFAULT_ERROR;
+		})
+
+		answer = currentAnswer;
+	});
+
+	return answer;
+}
+
+window.getAnswer = getAnswer;
