73ed95a3051c5b8de486f29e6845fe1e97eba838
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2021-12-19 18:28:32 +0300

    Added main ar functionality

---
+++ AR/Assets/_Core/Scripts/ARManager.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    public static event Action<bool> PoseValidChange;
+
+    public static ARManager Inst { get; private set; }
+
+    [SerializeField] GameObject _objectToSpawn;
+    [SerializeField] GameObject _indicator;
+
+    private GameObject _spawnedObj;
+    private Pose _pose;
+    private ARRaycastManager _raycastManager;
+    private bool _isPoseValid = false;
+
+    public static ARRaycastManager RaycastManager => Inst._raycastManager;
+    public static bool IsObjectSpawned => Inst._spawnedObj != null;
+    public static bool IsPoseValid
+    {
+        get => Inst._isPoseValid;
+        private set
+        {
+            if (Inst._isPoseValid == value) return;
+
+            Inst._isPoseValid = value;
+            PoseValidChange?.Invoke(value);
+        }
+    }
+
+    void Awake()
+    {
+        Inst = this;
+
+        _raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    public static void PlaceObject()
+    {
+        if (!IsPoseValid || Inst._spawnedObj) return;
+
+        var pose = Inst._pose;
+        Inst._spawnedObj = Instantiate(Inst._objectToSpawn, pose.position, pose.rotation);
+    }
+
+    public static void RemoveObject()
+    {
+        if (Inst._spawnedObj == null) return;
+        Destroy(Inst._spawnedObj);
+        Inst._spawnedObj = null;
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (_spawnedObj == null && _isPoseValid)
+        {
+            _indicator.SetActive(true);
+            _indicator.transform.SetPositionAndRotation(_pose.position, _pose.rotation);
+            return;
+        }
+
+        _indicator.SetActive(false);
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        if (IsPoseValid = _raycastManager.Raycast(screenCenter, hits, TrackableType.Planes))
+        {
+            _pose = hits[0].pose;
+        }
+    }
+}
---
+++ AR/Assets/_Core/Scripts/PlacementButton.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlacementButton : MonoBehaviour
+{
+    private TMP_Text _text;
+    private Button _button;
+
+    private bool _pressed = false;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<TMP_Text>();
+        _button = GetComponent<Button>();
+
+        _button.onClick.AddListener(OnClickHandler);
+        ARManager.PoseValidChange += OnPoseValidChangeHander;
+    }
+
+    private void Start()
+    {
+        OnPoseValidChangeHander(ARManager.IsPoseValid);
+    }
+
+    private void OnPoseValidChangeHander(bool isValid)
+    {
+        _button.interactable = isValid;
+    }
+
+    private void OnClickHandler()
+    {
+        _pressed = !_pressed;
+        _text.text = _pressed ? "Переместить" : "Установить";
+
+        if (_pressed)
+        {
+            ARManager.PlaceObject();
+        }
+        else
+        {
+            ARManager.RemoveObject();
+        }
+    }
+}
